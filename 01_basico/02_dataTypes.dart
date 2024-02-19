788615f7b0856b254a0cac9db06c8d1893f1718d
HectorARG <hectorr9577@gmail.com>
2024-02-19 16:55:31 -0600

    Tipado de datos: Numeros

---
+++ 01_basico/02_dataTypes.dart
@@ -0,0 +1,17 @@
+main() {
+  //  ===== Números
+  // var a = 10;
+  // var es un comodin es como el any de typescript
+  int a = 10;
+  double b = 5.5;
+
+  int? c;
+  // El signo ? quiere decir que la variable puede ser null
+
+  int _a = 30;
+  double $b = 40;
+
+  double resultado = _a + $b;
+
+  print(resultado);
+}
